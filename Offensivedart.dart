48ee1562c5abee4f9e4c843bd1510b8b63bdeaa6
theJraj <55058726+Jagarajdev@users.noreply.github.com>
2021-05-23 17:46:30 +0800

    Update Offensivedart.dart

--- Offensivedart.dart
+++ Offensivedart.dart
@@ -1,19 +1,14 @@
-// import 'dart:io';
+
 import 'dart:io';
-
-
 void main () {
-
 String message = stdin.readLineSync();
 bool containOffensive = message.contains('fuck');
 final finalMessage = message.replaceAll('fuck', '****');
-
 if ( containOffensive == true) {
-  print('You are using offensive');
+  print('You are using offensive word');
 } else {
   print('You are gentlemen');
 }
-
 print(finalMessage);
 
 
